f6a5e018ad209efadf73ed049713fdccb8ad002d
Kajetan Wierszelis (カイェタン  ヴィエルシェリス) <z523087@shibaura-it.ac.jp>
2024-01-23 07:07:04 +0900

    Implemented key features of the project (see extended desc)
    - C structure receiving and parsing
    - Game logic implemented (position tracking depending on key pressed, closing on quit)
    - server inter-process pipe communication for displaying status messages in server parent process.
    
    Note: This is the version as was presented on Monday's extended class.

--- client.c
+++ client.c
@@ -17,56 +17,75 @@
 	- System / C library programming manuals
 	https://cboard.cprogramming.com/c-programming/157438-capturing-keyboard-input-one-character-time.html
 	https://www.linuxquestions.org/questions/programming-9/detecting-arrow-key-presses-in-c-c-166055/
+	https://opensource.com/article/21/8/ncurses-linux
  */
 
+typedef struct {
+    char input[BUFSIZ];
+    time_t timestamp;
+    unsigned long checksum;
+} KeyboardInput;
+
+typedef struct {
+    int X;
+    int Y;
+    int score;
+    int port;
+} GameState;
+
 int main(int argc, char *argv[]) {
-	int sockfd, len;
-	char buf[BUFSIZ];
-	typedef struct {
-		char input[BUFSIZ];
-		time_t timestamp;
-		unsigned long checksum;
-	} KeyboardInput;
-	
-	struct sockaddr_in serv;
-	int port;
-	if (argc != 3) {
-		printf("Usage: ./client.elf host port\n");
-		exit(1);
-	}
-	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket");
-		exit(1);
-	}
-	serv.sin_family = PF_INET;
-	port = strtol(argv[2], NULL, 10);
-	serv.sin_port = htons(port);
-	inet_aton(argv[1], &serv.sin_addr);
-	if (connect(sockfd,(struct sockaddr *)&serv, sizeof(serv)) < 0) {
-		perror("connect");
-		exit(1);
-	}
+    int sockfd, len;
+    char buf[BUFSIZ];
+    
+    struct sockaddr_in serv;
+    int port;
+    if (argc != 3) {
+        printf("Usage: ./client.elf host port\n");
+        exit(1);
+    }
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    serv.sin_family = PF_INET;
+    port = strtol(argv[2], NULL, 10);
+    serv.sin_port = htons(port);
+    inet_aton(argv[1], &serv.sin_addr);
+    if (connect(sockfd,(struct sockaddr *)&serv, sizeof(serv)) < 0) {
+        perror("connect");
+        exit(1);
+    }
 
-	initscr();
-	raw();
-	noecho();
-	nonl();
-	keypad(stdscr, TRUE);
+    initscr();
+    raw();
+    noecho();
+    nonl();
+    keypad(stdscr, TRUE); /* Intercept arrow keys with flushing */
 
-	KeyboardInput ki;
-	char ch;
+    KeyboardInput ki;
+	GameState gs;
+    char ch;
 
-	while ((ch = getch()) != 'Q') {
-		ki.timestamp = time(NULL);
-		strncat(ki.input, &ch, 1);
-		ki.checksum = adler32(0, ki.input, strlen(ki.input));
-		len = send(sockfd, (char*)&ki, sizeof(ki), 0);
-		len = recv(sockfd, (char*)&ki, len, 0);
-		ki.input[len] = '\0';
-		printw("<== %s\n", ki.input);
-	}
+    srand(time(NULL));
+    int randomPort = rand() % (65535 - 1024 + 1) + 1024;
+    char portStr[6];
+    sprintf(portStr, "%d", randomPort);
+	move(25, 15);
+	printw("Connected to server on port %d\n", randomPort);
 
-	endwin();
-	close(sockfd);
+    while ((ch = getch()) != 'Q') {
+        ki.timestamp = time(NULL);
+        strncpy(ki.input, &ch, 1);
+		printf("Pressed key code %d at time %d", &ch, ki.timestamp);
+        ki.checksum = adler32(0, ki.input, strlen(ki.input));
+        len = send(sockfd, (char*)&ki, sizeof(ki), 0);
+        send(sockfd, portStr, strlen(portStr), 0);
+        len = recv(sockfd, (char*)&gs, sizeof(ki), 0);
+        ki.input[len] = '\0';
+		move(20, 10);
+        printw("Current position: (%d, %d), score %d, port %d\n", gs.X, gs.Y, gs.score, gs.port);
+    }
+    close(sockfd);
+    endwin();
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,72 +1,128 @@
-/* z523087 - based on code from slides */
+/* z523087 - Final project SnakeIT server, based on Class 9 socket server */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stddef.h> /* size_t */
+
+#include <stddef.h> /*size_t*/
+#include <ncurses.h> // for ki contents
+
+typedef struct {
+    char input[BUFSIZ];
+    time_t timestamp;
+    unsigned long checksum;
+} KeyboardInput;
+
+typedef struct {
+    int X;
+    int Y;
+    int score;
+    int port;
+} GameState;
 
 int main(int argc, char *argv[]) {
-	int sockfd, len, new_sockfd;
-	char buf[BUFSIZ];
-	struct sockaddr_in serv, clnt;
-	socklen_t sin_siz;
-	int port;
-	if (argc != 3) {
-		 printf("Usage: ./server.elf host port\n");
-		 exit(1);
-	}
-	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-		 perror("socket");
-		 exit(1);
-	}
-	printf("socket() called\n");
-	serv.sin_family = PF_INET;
-	port = strtol(argv[2], NULL, 10);
-	serv.sin_port = htons(port);
-	inet_aton(argv[1], &serv.sin_addr);
-	sin_siz = sizeof(struct sockaddr_in);
-	if (bind(sockfd, (struct sockaddr * ) &serv, sizeof(serv)) < 0) {
-		 perror("bind");
-		 exit(1);
-	}
-	printf("bind() called\n");
-	if (listen(sockfd, SOMAXCONN) < 0) {
-		 perror("listen");
-		 exit(1);
-	}
-	printf("listen() called\n");
-	
-	while (1) {
-		 if ((new_sockfd = accept(sockfd, (struct sockaddr *) &clnt, &sin_siz)) < 0) {
-			 perror("accept");
-		 }
-		 printf("connect from %s: %d\n", inet_ntoa(clnt.sin_addr), ntohs(clnt.sin_port));
-		 memset(buf, 0, BUFSIZ);
-	
-		 pid_t pid = fork();
-		 if (pid < 0) {
-			 perror("fork failed");
-			 exit(1);
-		 } else if (pid == 0) {
-			 close(sockfd);
-	
-			 while (strncasecmp(buf, "exit\n", 5) != 0) {
-				 len = recv(new_sockfd, buf, BUFSIZ, 0);
-				 buf[len] = '\0';
-				 printf("<== %s\n", buf);
-				 len = send(new_sockfd, buf, len, 0);
-			 }
-			 close(new_sockfd);
-			 exit(0);
-		 } else {
-			 close(new_sockfd);
-		 }
-	}
-	
-	close(sockfd);
-	return 0;
+    int sockfd, len, new_sockfd, pipefd[2];
+    char buf[BUFSIZ];
+    struct sockaddr_in serv, clnt;
+    socklen_t sin_siz;
+    int port;
+    if (argc != 3) {
+        printf("Usage: ./server.elf host port\n");
+        exit(1);
+    }
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    printf("socket() called\n");
+    serv.sin_family = PF_INET;
+    port = strtol(argv[2], NULL, 10);
+    serv.sin_port = htons(port);
+    inet_aton(argv[1], &serv.sin_addr);
+    sin_siz = sizeof(struct sockaddr_in);
+    if (bind(sockfd, (struct sockaddr * ) &serv, sizeof(serv)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+    printf("bind() called\n");
+    if (listen(sockfd, SOMAXCONN) < 0) {
+        perror("listen");
+        exit(1);
+    }
+    printf("listen() called\n");
+    
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        if ((new_sockfd = accept(sockfd, (struct sockaddr *) &clnt, &sin_siz)) < 0) {
+            perror("accept");
+        }
+        printf("connect from %s: %d\n", inet_ntoa(clnt.sin_addr), ntohs(clnt.sin_port));
+        memset(buf, 0, BUFSIZ);
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork failed");
+            exit(1);
+        } else if (pid == 0) {
+            close(sockfd);
+            close(pipefd[0]); /* Close read end of pipe in child */
+			
+			KeyboardInput ki;
+			len = recv(sockfd, (char*)&ki, sizeof(ki), 0);
+			char portStr[6];
+        	recv(sockfd, portStr, strlen(portStr), 0);
+            GameState gs;
+            char ch;
+            while (recv(new_sockfd, (char*)&ki, sizeof(ki), 0) > 0) { /* Receive KeyboardInput from client */
+				ch = ki.input[0]; /* Get the first character from the input */
+				switch (ch) {
+					case KEY_UP:
+						gs.Y--;
+						break;
+					case KEY_DOWN:
+						gs.Y++;
+						break;
+					case KEY_LEFT:
+						gs.X--;
+						break;
+					case KEY_RIGHT:
+						gs.X++;
+						break;
+					case 'Q':
+						close(pipefd[1]);
+            			exit(0);
+						break;
+				}
+
+				gs.port = atoi(portStr); /* Add player's port to GameState */
+                write(pipefd[1], (char*)&gs, sizeof(gs)); /* Write game state to pipe */
+				len = send(sockfd, (char*)&gs, sizeof(gs), 0); /* Send game state to client */
+            }
+            close(pipefd[1]); /* Close write end of pipe in child */
+            exit(0);
+        } else {
+            close(new_sockfd);
+        }
+    }
+
+    close(sockfd);
+    close(pipefd[1]); /* Close write end of pipe in parent */
+
+    GameState gs;
+    while (read(pipefd[0], (char*)&gs, sizeof(gs)) > 0) {
+        printf("Player position: (%d, %d) Port: %d\n", gs.X, gs.Y, gs.port);
+    }
+
+    close(pipefd[0]); /* Close read end of pipe in parent */
+    return 0;
 }
+
